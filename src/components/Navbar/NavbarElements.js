faf30a37b6792e2ce30bf81096701d2c87169a21
mgcarbonell <mgcarbonell@gmail.com>
2021-01-07 23:43:54 -0800

    Adding onclikcs

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
-import { Link as LinkRouter } from 'react-router-dom';
 import { Link as LinkScroll } from 'react-scroll';
+import { Link as LinkRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const Nav = styled.nav`
-  background: #000;
+  background: ${({ scrollNav }) => (scrollNav ? '#000' : 'transparent')};
   height: 80px;
-  ${'' /* margin-top: -80px; */}
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -17,7 +17,7 @@ export const Nav = styled.nav`
   @media screen and (max-width: 960px) {
     transition: 0.8s all ease;
   }
-`
+`;
 
 export const NavContainer = styled.div`
   display: flex;
@@ -27,34 +27,33 @@ export const NavContainer = styled.div`
   width: 100%;
   padding: 0 24px;
   max-width: 1100px;
-`
+`;
 
 export const Logo = styled(LinkRouter)`
-  color: #cc0000;
-  display: flex;
+  color: #fff;
   justify-self: flex-start;
   cursor: pointer;
   font-size: 1.5rem;
+  display: flex;
   align-items: center;
   margin-left: 24px;
   font-weight: bold;
   text-decoration: none;
-`
+`;
 
 export const MobileIcon = styled.div`
   display: none;
-  
-  @media screen and (max-width: 768px){
+
+  @media screen and (max-width: 768px) {
     display: block;
     position: absolute;
     top: 0;
     right: 0;
-    transform: trasnlate(-100%, 60%);
+    transform: translate(-100%, 60%);
     font-size: 1.8rem;
     cursor: pointer;
-    color: #fff;
   }
-`
+`;
 
 export const NavMenu = styled.ul`
   display: flex;
@@ -66,11 +65,21 @@ export const NavMenu = styled.ul`
   @media screen and (max-width: 768px) {
     display: none;
   }
-`
+`;
 
 export const NavItem = styled.li`
   height: 80px;
-`
+`;
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
 export const NavLinks = styled(LinkScroll)`
   color: #fff;
   display: flex;
@@ -81,6 +90,7 @@ export const NavLinks = styled(LinkScroll)`
   cursor: pointer;
 
   &.active {
-    border-bottom: 3px solid #cc0000;
+    border-bottom: 3px solid #01bf71;
   }
-`
+`;
+
