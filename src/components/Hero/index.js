d98ad372966da9e3a11f4b15e4e60e87df9f295d
mgcarbonell <mgcarbonell@gmail.com>
2021-01-09 15:09:21 -0800

    Hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,7 +4,12 @@ import {
   HeroContainer,
   HeroBg,
   VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
 } from './HeroElements';
+import Typical from 'react-typical';
+import Zoom from 'react-reveal/Zoom'
 
 const Hero = () => {
   return (
@@ -12,6 +17,34 @@ const Hero = () => {
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
       </HeroBg>
+      <HeroContent>
+        <Zoom>
+          <HeroH1>
+            Hi! It's me, Mario!
+          </HeroH1>
+        </Zoom>
+        <HeroP> I'm{' '}
+
+          <Typical 
+            loop={Infinity}
+            wrapper="b" 
+            steps={[
+              "a software engineer",
+              1000,
+              "a fullstack developer",
+              1000,
+              "an open sourcer",
+              1000,
+              "a web developer",
+              1000,
+              "intensely curious",
+              1000,
+              "looking to the next tech horizon",
+              3000,
+            ]}
+          />
+        </HeroP>
+      </HeroContent>
     </HeroContainer>
   )
 }
