cc2fbd13b289a5afceaa7071954c5bea00aa4fed
mgcarbonell <mgcarbonell@gmail.com>
2021-01-10 20:56:43 -0800

    Quick simple contact

--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -1,10 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from '../ButtonElements'
+import {
+  ConnectContainer,
+  ConnectWrapper,
+  ConnectRow,
+  Column1,
+  ImgWrap,
+  Img,
+  BtnWrap,
+  ArrowDown,
+  ArrowDownward,
+  Column2,
+  IconLink
+} from './ConnectElements';
+import Gmail from '../../icons/gmail.png';
+import Twitter from '../../icons/013-twitter-1.png';
+import Linkedin from '../../icons/031-linkedin.png';
+import Github from '../../icons/039-github.png';
+import HackerRank from '../../icons/HackerRank_logo.png';
+import LeetCode from '../../icons/LeetCode_logo_black.png';
 
 const Connect = () => {
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  };
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <ConnectContainer id="connect">
+        <ConnectWrapper>
+          <ConnectRow>
+            <Column1>
+              <ImgWrap>
+                <IconLink>
+                  <Img src={Gmail}/>
+                </IconLink>
+                <IconLink>
+                  <Img src={Github}/>
+                </IconLink>
+                <IconLink>
+                  <Img src={Linkedin}/>
+                </IconLink>
+                <BtnWrap>
+                    <Button to="projects"
+                      smooth={true}
+                      duration={500}
+                      spy={true}
+                      offset={-80}
+                      primary="true"
+                      dark="true"
+                      onMouseEnter={onHover}
+                      onMouseLeave={onHover}
+                    >
+                      Back To Top
+                      { hover ? <ArrowDownward /> : <ArrowDown /> }
+                    </Button>
+                  </BtnWrap>
+              </ImgWrap>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <IconLink>
+                  <Img src={Twitter}/>
+                </IconLink>
+                <IconLink>
+                  <Img src={HackerRank}/>
+                </IconLink>
+                <IconLink>
+                  <Img src={LeetCode}/>
+                </IconLink>
+              </ImgWrap>
+            </Column2>
+          </ConnectRow>
+        </ConnectWrapper>
+      </ConnectContainer>
+    </>
   )
 }
 
