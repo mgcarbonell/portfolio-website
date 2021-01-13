db011fcfe9302a9b4e60a1e1b3ec5b1227cf2760
Mario Carbonell <57305940+mgcarbonell@users.noreply.github.com>
2021-01-12 16:40:50 -0800

    Update AboutData.js

--- src/components/About/AboutData.js
+++ src/components/About/AboutData.js
@@ -6,7 +6,7 @@ export const aboutObj = {
   topLine: 'About Mario',
   headline: 'Software Engineer',
   paragraph1: `Welcome to my portfolio! I'm glad you're here! I have always had a deep love for technology; from taking Cisco networking in high school, to building my own PCs. However, this wasn't enough for me, I needed more! Learning how to program was something that I always wanted to do, but I never thought I could do it.`, 
-  paragraph2: `In September of 2020 as a personal challenge to myself I signed up for a bootcamp on Software Engineering from General Assembly. With a lot of hard work and th growth mindset of "I can do it, I just can't do it yet" I graduated in December of 2020 as a fullstack software engineer! It goes to show that if I put my mind to something I can do it!`,
+  paragraph2: `In September of 2020 as a personal challenge to myself I signed up for a bootcamp on Software Engineering from General Assembly. With a lot of hard work and the growth mindset of "I can do it, I just can't do it yet" I graduated in December of 2020 as a fullstack software engineer! It goes to show that if I put my mind to something I can do it!`,
   buttonLabel: 'My Toolbox',
   imgStart: false,
   alt: 'Photo of Mario',
